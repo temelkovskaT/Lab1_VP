2e31e2c431b1b4d944d56983fa39549ae23d51a3
Tea <tea.temelkovska410@gmail.com>
2022-11-12 22:27:28 +0100

    filter test

---
+++ src/main/java/mk/ukim/finki/wp/lab/web/filter/CourseFilter.java
@@ -0,0 +1,38 @@
+package mk.ukim.finki.wp.lab.web.filter;
+
+import mk.ukim.finki.wp.lab.model.Course;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@WebFilter
+public class CourseFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        String path = request.getServletPath();
+        Long courseId = (Long)request.getSession().getAttribute("courseChosen");
+        if(courseId==null && !path.equals("/listCourses")){
+            response.sendRedirect("/listCourses");
+        }else{
+            filterChain.doFilter(servletRequest,servletResponse);
+        }
+
+    }
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
+    }
+}
